d8d3378ae75f069d85a5430ac2f0d503baf8a22b
pierre <80222572+pierre-robspierre@users.noreply.github.com>
2021-03-07 18:26:28 -0500

    Create Clan.java

---
+++ Clan.java
@@ -0,0 +1,11 @@
+import //paper
+  
+  public class Clan {
+    
+    public String clan_name
+    
+    public Clan(String name, /*player who gave command, leader */ ){           //clan constructor
+      name = clan_name;
+      
+    }
+    
