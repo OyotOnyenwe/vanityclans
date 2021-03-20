e45e0e4dd371bb12e3d0f89cc4da6f8b28ed1450
Oyot Onyenwe <oyotonyenwe@gmail.com>
2021-03-20 18:07:07 -0400

    updated plugin.yml

--- vanityclans/src/main/java/com/onyenwe/vanityclans/App.java
+++ vanityclans/src/main/java/com/onyenwe/vanityclans/App.java
@@ -3,14 +3,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class App extends JavaPlugin
 {
+    String pluginVersion = getDescription().getVersion();
+    String pluginName = getName();
+    
     @Override
     public void onEnable()
     {
-        getLogger().info("VanityClans v0.0.1 has been enabled.");
+        getLogger().info("VanityClans has been enabled.");
     }
     @Override
     public void onDisable()
     {
-        getLogger().info("VanityClans v0.0.1 has been disabled.");
+        getLogger().info("VanityClans has been disabled.");
     }
 }
